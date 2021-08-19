8ef7e3b6fc47a87ad3e08cbc5a0a9274585c1dfe
westhof91 <88407013+westhof91@users.noreply.github.com>
2021-08-19 13:33:59 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code in this file!
+const currentUser = 'Westhof';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!` ;
+console.log(excitedWelcomeMessage)
